3876a4771e1dc7673ccb17ec7752c4f6c7685688
vuhayhat <vuhayhatmtp@gmail.com>
2024-02-03 22:57:25 +0700

    dangphattrien

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,12 @@
 # Trong shop/urls.py
 from django.urls import path
 from .views import login_view  # Đổi tên import để sử dụng login_view
-
+from .views import register_view
 urlpatterns = [
     path('', login_view, name='login'),  # Sử dụng login_view thay vì your_default_view
     path('login/', login_view, name='login'),  # Giữ lại nếu cần thiết
+    path('', register_view, name='register'),
+    path('register/', register_view, name='register'),
+
     # Các đường dẫn khác nếu có
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'login.html')
+def register_view(request):
+    # Xử lý đăng ký ở đây nếu cần
+    return render(request, 'register.html')
